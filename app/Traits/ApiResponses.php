f8acbc92db7deae312ebd9878408afee080e7cdd
Dheeraj Kumar <dheeraj.webdesky@gmail.com>
2025-07-29 03:05:23 +0530

    api response codes

--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 trait ApiResponses{
 
     protected function ok($message){
-        return $this->success($message);
+        return $this->success($message, 200);
     }
     protected function success($message, $statusCode = 200)
     {
